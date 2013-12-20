73132f50c8e0a5f3ac58a05d4aa1bac559451331
rejuvyesh <mail@rejuvyesh.com>
2013-12-20 12:43:00 +0530

    class

--- lib/rmpv/trakt.rb
+++ lib/rmpv/trakt.rb
@@ -10,20 +10,22 @@ require "traktr"
 require "yaml"
 
 module Rmpv
-  def self.traktscrobble show
-    traktconfig = YAML.load File.open("#Dir.home/.traktrc")
-    @trakt = Trakt::Client.new(traktconfig["api_key"], traktconfig["username"], traktconfig["password"], true)
-    tries = 5
-    begin
-      info = @trakt.search.shows(show.name)
-      @episodes = @trakt.show.season(info[0].title, show.series)
-      res = @trakt.show.episode.seen(info[0], @episodes[show.episode-1..show.episode-1])
-    rescue Exception => e
-      tries -= 1
-      if tries > 0
-        retry
-      else
-        puts "Couldn't connect to trakt servers"
+  class Trakt
+    def self.traktscrobble show
+      traktconfig = YAML.load File.open("#Dir.home/.traktrc")
+      @trakt = Trakt::Client.new(traktconfig["api_key"], traktconfig["username"], traktconfig["password"], true)
+      tries = 5
+      begin
+        info = @trakt.search.shows(show.name)
+        @episodes = @trakt.show.season(info[0].title, show.series)
+        res = @trakt.show.episode.seen(info[0], @episodes[show.episode-1..show.episode-1])
+      rescue Exception => e
+        tries -= 1
+        if tries > 0
+          retry
+        else
+          puts "Couldn't connect to trakt servers"
+        end
       end
     end
   end
