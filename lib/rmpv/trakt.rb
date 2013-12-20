636d8b12679e463ac75dca395c926ace7d323588
rejuvyesh <mail@rejuvyesh.com>
2013-12-20 11:14:43 +0530

    wrote trakt module

---
+++ lib/rmpv/trakt.rb
@@ -0,0 +1,30 @@
+#!/usr/bin/env ruby
+# -*- encoding: utf-8 -*-
+#
+# File: trakt.rb
+#
+# Copyright rejuvyesh <mail@rejuvyesh.com>, 2013
+# License: GNU GPL 3 <http://www.gnu.org/copyleft/gpl.html>
+
+require "traktr"
+require "yaml"
+
+module Rmpv
+  def self.traktscrobble show
+    traktconfig = YAML.load File.open("#Dir.home/.traktrc")
+    @trakt = Trakt::Client.new(traktconfig["api_key"], traktconfig["username"], traktconfig["password"], true)
+    tries = 5
+    begin
+      info = @trakt.search.shows(show.name)
+      @episodes = @trakt.show.season(info[0].title, show.series)
+      res = @trakt.show.episode.seen(info[0], @episodes[show.episode-1..show.episode-1])
+    rescue Exception => e
+      tries -= 1
+      if tries > 0
+        retry
+      else
+        puts "Couldn't connect to trakt servers"
+      end
+    end
+  end
+end
