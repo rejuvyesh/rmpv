ad6573c5d52d52cba785102db6767d3c54d7ee50
rejuvyesh <mail@rejuvyesh.com>
2013-12-20 12:21:19 +0530

    parse options, find flags to run

---
+++ lib/option.rb
@@ -0,0 +1,77 @@
+#!/usr/bin/env ruby
+# -*- encoding: utf-8 -*-
+#
+# File: option.rb
+#
+# Copyright rejuvyesh <mail@rejuvyesh.com>, 2013
+
+require "optparse"
+
+module Rmpv
+  def self.parse args,cmd, options
+    begin
+      OptionParser.new do |opts|
+        opts.banner = "Usage: rmpv [options]"
+
+        opts.on("-+", "--vol=VOL", Integer, "increase volume by VOL") do |v|
+          options[:volume]   = v
+        end
+
+        opts.on("-l", "--top-left",     "play in top-left corner of the screen") do |tl|
+          options[:position] = :top_left
+        end
+        opts.on("-b", "--bottom-right", "play in bottom-right corner of the screen") do |br|
+          options[:position] = :bottom_right
+        end
+        opts.on("-x", "--speed SPEED",  Float, "increase speed by SPEED") do |x|
+          options[:speed]    = x
+        end
+        opts.on("-s", "--size STR",     "set size of the player") do |sa| 
+          options[:size]     = sa
+        end
+        opts.on("-y", "--youtube",      "youtube mode") do |y|
+          options[:speed]    = 1.5
+          cmd << "--cache-default=2048"
+        end
+        opts.on("-a", "--audio",        "audio mode") do |a|
+          cmd << "--audio-display=no --gapless-audio"
+        end
+        opts.on("-t", "--trakt",        "scrobble to trakt") do |t| 
+          options[:trakt]    = true;
+        end
+      end.parse!
+
+    rescue OptionParser::InvalidOption => e
+      cmd << e.to_s.sub(/^invalid option:\s+/, "")
+    end
+
+    return cmd, options
+  end
+
+  def self.command cmd, options
+    # size
+    if options[:size]
+      cmd << "--autofit='#{options[:size]}'"
+    end
+
+    if options[:position] == :top_left
+      cmd << "--geometry='0:17'"
+    elsif options[:position] == :bottom_right
+      cmd << "--geometry='100%:97%'"
+    end
+
+    # audio filter
+    # Remove chirping at higher speed
+    cmd << "--af=scaletempo"  
+    if options[:volume].nonzero?
+      cmd << "--af=volume=#{options[:volume]}"
+    end
+
+    # speed increase
+    if options[:speed] != 1.0
+      cmd << "--speed=#{options[:speed]}"
+    end
+
+    return cmd
+  end
+end
