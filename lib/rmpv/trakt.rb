e95210fd78bd7c615c8e76ea3cbfeea481f4c18f
rejuvyesh <mail@rejuvyesh.com>
2013-12-22 17:49:08 +0530

    support trakt checkin now

--- lib/rmpv/trakt.rb
+++ lib/rmpv/trakt.rb
@@ -7,6 +7,7 @@
 
 require "traktr"
 require "yaml"
+require "rmpv/version"
 
 module Rmpv
   class Trakt
@@ -26,15 +27,16 @@ module Rmpv
       tries = 5
       begin
         info = @trakt.search.shows(show.name)
-        @episodes = @trakt.show.season(info[0].title, show.series)
-        res = @trakt.show.episode.seen(info[0], @episodes[show.episode-1..show.episode-1])
-        puts "Scrobbled to trakt (Y)"
+        # @episodes = @trakt.show.season(info[0].title, show.series)
+        # res = @trakt.show.episode.seen(info[0], @episodes[show.episode-1..show.episode-1])
+        res = @trakt.show.checkin(info[0], show.series, show.episode, Rmpv::VERSION, "Dec 22 2013")
+        puts "Checked-in to trakt (Y)"
       rescue Exception => e
         tries -= 1
         if tries > 0
           retry
         else
-          puts "Couldn't connect to trakt servers: #{res}"
+          puts "Couldn't connect to trakt servers: #{e}, #{res}"
         end
       end
     end
