accf5e6d0d5e47345125722b162647e0d455e68c
rejuvyesh <mail@rejuvyesh.com>
2013-12-24 23:49:41 +0530

    move variables outside; info returned contains runtime;\n scrobble if >= 50%

--- lib/rmpv/trakt.rb
+++ lib/rmpv/trakt.rb
@@ -37,17 +37,18 @@ module Rmpv
           puts "Couldn't connect to trakt servers: #{e}, #{res}"
         end
       end
-      info[0].duration
+      info[0].runtime # return duration
+      
     end
     
     ##
     # Scrobble the show
     
-    def scrobble show
+    def scrobble show, progress
       tries = 5
       begin
         info = @trakt.search.shows(show.name)
-        res = @trakt.show.scrobble(info[0], show.series, show.episode, 85, Rmpv::VERSION, "Dec 22 2013")
+        res = @trakt.show.scrobble(info[0], show.series, show.episode, progress, Rmpv::VERSION, "Dec 22 2013")
         puts "Scrobbled to trakt (Y)"
       rescue Exception => e
         tries -= 1
