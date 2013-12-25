9114a105bc38672675f55a28e1a3c04f5696ed56
rejuvyesh <mail@rejuvyesh.com>
2013-12-25 22:31:37 +0530

    parse file name

--- lib/rmpv/myanimelist.rb
+++ lib/rmpv/myanimelist.rb
@@ -14,18 +14,30 @@ module Rmpv
     ##
     # Initialize the Myanimelist client
 
-    def initialize
-      myanimeconfig = YAML.load File.open('#{Dir.home}/.rmpvrc')
+    def initialize file
+      myanimeconfig = YAML.load File.open("#{Dir.home}/.rmpvrc")
       MyAnimeList.configure do |config|
         config.username = myanimeconfig["myanimelist"]["username"]
         config.password = myanimeconfig["myanimelist"]["password"]
       end
     end
 
-    def scrobble show
+    def parse file
+      raw_name = file
+      raw_name = raw_name[0, raw_name.rindex(/\./)] # remove extension
+      raw_name = raw_name.gsub(/(\.|_|\-)/, '')  #Chars used in filenames as a substitute for spaces
+      raw_name = raw_name.gsub(/\(.*?\)/, '') # Remove anything surrounded by paranthesis
+      raw_name = raw_name.gsub(/\[.*?\]/, '') # Remove anything surrounded by paranthesis
+      ep = /(\d+)/.match(raw_name)
+      name = raw_name.gsub(/(\d+)/, '')
+      episode = ep.nil? ? 1 : ep[1].to_i
+      return name.strip!, episode
+    end
+
+    def scrobble anime, ep
       tries = 5
       begin
-        
+        info = MyAnimeList.search_anime(anime)
       rescue Exception => e
         tries -= 1
         if tries > 0
