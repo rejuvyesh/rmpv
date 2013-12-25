c53ba827aef69719571207d355423743c4d407a3
rejuvyesh <mail@rejuvyesh.com>
2013-12-25 16:14:04 +0530

    sorry forgot

--- lib/rmpv/myanimelist.rb
+++ lib/rmpv/myanimelist.rb
@@ -16,13 +16,10 @@ module Rmpv
 
     def initialize
       myanimeconfig = YAML.load File.open('#{Dir.home}/.rmpvrc')
-<<<<<<< Updated upstream
-=======
       MyAnimeList.configure do |config|
         config.username = myanimeconfig["myanimelist"]["username"]
         config.password = myanimeconfig["myanimelist"]["password"]
       end
->>>>>>> Stashed changes
     end
 
     def scrobble show
