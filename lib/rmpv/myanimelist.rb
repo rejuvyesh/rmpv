97c35998c18eaa042964e396b4aed7267f4e4b45
rejuvyesh <mail@rejuvyesh.com>
2013-12-25 15:39:06 +0530

    add myanimelist

--- lib/rmpv/myanimelist.rb
+++ lib/rmpv/myanimelist.rb
@@ -5,6 +5,31 @@
 #
 # Copyright rejuvyesh <mail@rejuvyesh.com>, 2013
 
+require "yaml"
+require "myanimelist"
+
 module Rmpv
-  
+  class Myanimelist
+
+    ##
+    # Initialize the Myanimelist client
+
+    def initialize
+      myanimeconfig = YAML.load File.open('#{Dir.home}/.rmpvrc')
+    end
+
+    def scrobble show
+      tries = 5
+      begin
+        
+      rescue Exception => e
+        tries -= 1
+        if tries > 0
+          retry
+        else
+          puts "Couldn't connect to Myanimelist servers: #{e}"
+        end
+      end
+    end
+  end
 end
